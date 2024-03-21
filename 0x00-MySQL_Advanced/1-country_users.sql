3eaa599470c5978a358580cfa9dad484c2efce16
boak123 <boluwatifeakorede17@gmail.com>
2024-03-21 22:17:57 +0000

    enum

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,2 @@
 -- script that creates a table users
-CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, email varchar(255) NOT NULL UNIQUE, name varchar(255), country ENUM(US, CO, TN) NOT NULL)
+CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, email varchar(255) NOT NULL UNIQUE, name varchar(255), country ENUM('US', 'CO', 'TN') NOT NULL);
