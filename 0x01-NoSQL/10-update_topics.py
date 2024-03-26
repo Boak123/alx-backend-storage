b748893998b11775f020baaa8334bfcd04122c50
boak123 <boluwatifeakorede17@gmail.com>
2024-03-26 10:27:45 +0000

    update document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -14,4 +14,4 @@ def update_topics(mongo_collection, name, topics):
     :param topics:
     :return:
     """
-    mongo_collection.updatemany({"name": "name"}, {$set: {"topics": "topics"}})
+    mongo_collection.update_many({"name": "name"}, {"$set": {"topics": "topics"}})
