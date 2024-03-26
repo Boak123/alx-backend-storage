75b8f5f74ed0973d2d3c09ae275fb58ade611ac4
boak123 <boluwatifeakorede17@gmail.com>
2024-03-26 14:04:59 +0000

    lists

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 
+Where can i learn python
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function that returns the list of school
+
+    :param mongo_collection:
+    :param topic:
+    :return:
+    """
+    retutn mongo_collection.find({"topics": "topic"})
